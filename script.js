c275f678fedc11cdaa727fcaf3d64fcc8cc3c991
gajaguerrini <27884679-gajaguerrini@users.noreply.replit.com>
2023-11-08 11:29:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var item;
+var nameOfTable;
+
+function Table(tablename, tablecontetnt) {
+  this.Name = tablename
+  this.Array = tablecontent
+}
+
+function clickGetItem() {
+    var listOfItems =  document.getElementById("textbox").value
+    nameOfTable = document.getElementById("fname").value
+    console.log(nameOfTable)
+    const array = listOfItems.split(/\r?\n/);
+    console.log(array)
+    item = array[Math.floor(Math.random()*array.length)];
+    console.log(item)
+    document.getElementById("item").innerHTML = item;
+    
+}
