e4c1badb67f85055602c2b8a7c4fc6f91a1cf7ec
GajaGuerrini <127405083+GajaGuerrini@users.noreply.github.com>
2023-11-23 11:42:45 +0000

    nesting feature completed

--- script.js
+++ script.js
@@ -1,60 +1,66 @@
-document.addEventListener('click', function (event) {
-    if (event.target.classList.contains('getItemButton')) {
-        clickGetItem(event.target);
-    }
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('getItemButton')) {
+    clickGetItem(event.target, 0);
+  }
 });
 
 function getFnamesandButtons() {
-    // Assume all containers have the class "itemboxbox"
-    var containers = document.querySelectorAll('.itembox');
-    var allFnamesButtons = [];
-    // Loop through each container and get the id="fname" element
-    containers.forEach(function(container) {
-        var fnameElement = container.querySelector("#fname");
-        var buttonElement = container.querySelector(".getItemButton");
-        if (fnameElement && buttonElement) {
-            var containerValues = {
-                fname: fnameElement.value,
-                button: buttonElement
-            };
+  // Assume all containers have the class "itemboxbox"
+  var containers = document.querySelectorAll('.itembox');
+  var allFnamesButtons = [];
+  // Loop through each container and get the id="fname" element
+  containers.forEach(function(container) {
+    var fnameElement = container.querySelector("#fname");
+    var buttonElement = container.querySelector(".getItemButton");
+    if (fnameElement && buttonElement) {
+      var containerValues = {
+        fname: fnameElement.value,
+        button: buttonElement
+      };
 
-          allFnamesButtons.push(containerValues);
-        }
-    });
+      allFnamesButtons.push(containerValues);
+    }
+  });
 
-    return allFnamesButtons;
+  return allFnamesButtons;
 }
 
 
 
-function clickGetItem(button) {
+function clickGetItem(button, nestingcounter) {
+  nestingcounter++;
   var container = button.closest('.itembox');
   var listOfItems = container.querySelector("#textbox").value;
   var nameOfTable = container.querySelector("#fname").value;
 
   const array = listOfItems.split(/\r?\n/);
-  var item = array[Math.floor(Math.random()*array.length)];
-  var fnames = getFnamesandButtons();
+  var item = array[Math.floor(Math.random() * array.length)];
   container.querySelector("#item").innerHTML = nameOfTable + " : " + item;
+
   var allValues = getFnamesandButtons();
-  console.log(typeof(allValues))
   allValues.forEach(function(element) {
-      console.log("fname:", element.fname);
-      console.log("button:", element.button);
+    //console.log("fname:", element.fname);
+    //console.log("button:", element.button);
+    console.log("count: ", nestingcounter);
+    if (item == element.fname && nestingcounter < 20) {
+      //console.log(element.fname);
+      //console.log(element.button);
+      clickGetItem(element.button, nestingcounter);
+    }
   });
-  
+
 }
 
 function run() {
-    var itembox = document.getElementById("itemboxbox");
-    var newItembox = itembox.cloneNode(true);
+  var itembox = document.getElementById("itemboxbox");
+  var newItembox = itembox.cloneNode(true);
 
-    // Clear the input and output fields in the new itembox
-    newItembox.querySelector("#fname").value = "";
-    newItembox.querySelector("#textbox").value = "";
-    newItembox.querySelector("#getitem").innerHTML = "";
-    newItembox.querySelector("#item").innerHTML = "";
+  // Clear the input and output fields in the new itembox
+  newItembox.querySelector("#fname").value = "";
+  newItembox.querySelector("#textbox").value = "";
+  newItembox.querySelector("#getitem").innerHTML = "";
+  newItembox.querySelector("#item").innerHTML = "";
 
-    // Append the new itembox to the document
-    document.body.appendChild(newItembox);
+  // Append the new itembox to the document
+  document.body.appendChild(newItembox);
 }
